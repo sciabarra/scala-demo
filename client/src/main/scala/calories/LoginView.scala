e816ee5ca75192bf64d48efc60ea0d693c466e19
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-12-19 15:41:37 +0100

    adding some sanity check to registration

--- client/src/main/scala/calories/LoginView.scala
+++ client/src/main/scala/calories/LoginView.scala
@@ -1,6 +1,8 @@
 package calories
 
 import diode.{Dispatcher, ModelR}
+import org.scalajs.dom
+import scala.util.Try
 import scalatags.JsDom.all._
 import org.scalajs.jquery.{jQuery => $}
 
@@ -15,7 +17,8 @@ class LoginView(message: ModelR[String], dispatch: Dispatcher) {
 
   def splash = div(cls := "jumbotron",
     h1("Calories Counter"),
-    h3("""On a diet? Register here,set your daily calories target
+    h3(
+      """On a diet? Register here,set your daily calories target
       and keep your daily dose of calories under control."""))
 
   def note = {
@@ -56,19 +59,19 @@ class LoginView(message: ModelR[String], dispatch: Dispatcher) {
     p("If you  dot not have yet an account."),
 
     div(cls := "form-group",
-      input(id := "name", placeholder := "Full Name", `type` := "text",
+      input(id := "calories", placeholder := "Daily Calories Target", `type` := "number",
         cls := "form-control", required := "")),
     div(cls := "form-group",
-      input(id := "calories", placeholder := "Daily Calories Target", `type` := "number",
+      input(id := "name", placeholder := "Full Name", `type` := "text",
         cls := "form-control", required := "")),
     div(cls := "form-group",
       input(id := "username", placeholder := "Login", `type` := "text",
         cls := "form-control", required := "")),
     div(cls := "form-group",
-      input(id := "password1", placeholder := "Password", `type` := "text",
+      input(id := "password1", placeholder := "Password", `type` := "password",
         cls := "form-control", required := "")),
     div(cls := "form-group",
-      input(id := "password2", placeholder := "Repeat Password", `type` := "text",
+      input(id := "password2", placeholder := "Repeat Password", `type` := "password",
         cls := "form-control", required := "")),
     div(cls := "form-group",
       button("Register", `type` := "button",
@@ -76,16 +79,23 @@ class LoginView(message: ModelR[String], dispatch: Dispatcher) {
         onclick := { () =>
           val password1 = $("#password1").value().toString
           val password2 = $("#password2").value().toString
-          dispatch(if (password1.equals(password2))
-            Register(
-              name = $("#name").value().toString,
-              username = $("#username").value().toString,
-              calories = Integer.parseInt($("#calories").value().toString),
-              password = password1)
-          else
-            LoggedUser(Left("Passwords do not match!")))
-        }
-      )))
+          val name = $("#name").value().toString
+          val username = $("#username").value().toString
 
+          val calories = Try(Integer.parseInt($("#calories").value().toString))
+
+          if (!password1.equals(password2)) {
+            dom.alert("Passwords don't match!")
+          } else if (calories.isFailure || calories.get < 100 || calories.get > 10000) {
+            dom.alert("Please, provide a reasonable number of calories!")
+          } else if (name.trim.size == 0 || username.trim.size == 0 || password1.size == 0) {
+            dom.alert("Please fill all the fields!")
+          } else {
+            dispatch(Register(name = name,
+              username = username,
+              calories = calories.get,
+              password = password1))
+          }
+        })))
 }
 
